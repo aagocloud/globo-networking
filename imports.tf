da7aa41088467ca907e327e7961496076f3e9e80
aagocloud <pubudu.senadeera@gmail.com>
2024-08-28 12:43:38 +0100

    Initial commit

---
+++ imports.tf
@@ -0,0 +1,44 @@
+/*# import {
+#  to = module.main.aws_vpc.this[0]
+#  id = "vpc-0b2e8ca87d3747c10"
+}
+
+import {
+  to = module.main.aws_subnet.public[0]
+  id = "subnet-08781556ab2baf394"
+}
+
+import {
+  to = module.main.aws_subnet.public[1]
+  id = "subnet-05a130cc568e42554"
+}
+
+import {
+  to = module.main.aws_internet_gateway.this[0]
+  id = "igw-0c45dd18a2a758630"
+}
+
+import {
+  to = module.main.aws_route.public_internet_gateway[0]
+  id = "rtb-06560fecb2a3c51e6_0.0.0.0/0"
+}
+
+import {
+  to = module.main.aws_route_table.public[0]
+  id = "rtb-06560fecb2a3c51e6"
+}
+
+import {
+  to = module.main.aws_route_table_association.public[0]
+  id = "subnet-08781556ab2baf394/rtb-06560fecb2a3c51e6"
+}
+
+import {
+  to = module.main.aws_route_table_association.public[1]
+  id = "subnet-05a130cc568e42554/rtb-06560fecb2a3c51e6"
+}
+
+import {
+  to = aws_security_group.ingress
+  id = "sg-0b339d0882792937b"
+} */
---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "region" {
+    type = string
+    description = "default region for aws resources"
+    default = "us-east-1"
+}
+
+variable "prefix" {
+  type = string
+  description = "value of the VPC"
+  default = "globo-dev"
+}
+
+variable "environment" {
+  type = string
+  description = "envrironment details"
+  default = "development"
+}
+
+variable "cide_block" {
+    type = string
+    description = "The CIDR block for the VPC"
+    default = "10.42.0.0/16"
+  
+}
+
+variable "public_subnets" {
+  type = map(string)
+  description = "List of public subnets inside VPC"
+  default = {
+    public-1 = "10.42.10.0/24"
+    public-2 = "10.42.11.0/24"
+      }
+}
