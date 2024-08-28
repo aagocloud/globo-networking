1b697bd1a14ebded059c0019b39a6a06576d80ac
aagocloud <pubudu.senadeera@gmail.com>
2024-08-28 13:56:53 +0100

    Fix formatting Issues

--- resources.tf
+++ resources.tf
@@ -25,18 +25,18 @@ locals {
 
 
 module "main" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
   version = "5.13.0"
 
   name = var.prefix
   cidr = var.cide_block
 
-  azs             = slice(data.aws_availability_zones.available.names, 0, length(var.public_subnets)) 
-  public_subnets  = [for k,v in var.public_subnets : v]
-  public_subnet_names = [for k,v in var.public_subnets : "${var.prefix}-${k}"]
-  enable_dns_hostnames = true
-  public_subnet_suffix = ""
-  public_route_table_tags = { Name = "${var.prefix}-public"}
+  azs                     = slice(data.aws_availability_zones.available.names, 0, length(var.public_subnets))
+  public_subnets          = [for k, v in var.public_subnets : v]
+  public_subnet_names     = [for k, v in var.public_subnets : "${var.prefix}-${k}"]
+  enable_dns_hostnames    = true
+  public_subnet_suffix    = ""
+  public_route_table_tags = { Name = "${var.prefix}-public" }
   map_public_ip_on_launch = true
 
   enable_nat_gateway = false
--- variables.tf
+++ variables.tf
@@ -1,33 +1,33 @@
 variable "region" {
-    type = string
-    description = "default region for aws resources"
-    default = "us-east-1"
+  type        = string
+  description = "default region for aws resources"
+  default     = "us-east-1"
 }
 
 variable "prefix" {
-  type = string
+  type        = string
   description = "value of the VPC"
-  default = "globo-dev"
+  default     = "globo-dev"
 }
 
 variable "environment" {
-  type = string
+  type        = string
   description = "envrironment details"
-  default = "development"
+  default     = "development"
 }
 
 variable "cide_block" {
-    type = string
-    description = "The CIDR block for the VPC"
-    default = "10.42.0.0/16"
-  
+  type        = string
+  description = "The CIDR block for the VPC"
+  default     = "10.42.0.0/16"
+
 }
 
 variable "public_subnets" {
-  type = map(string)
+  type        = map(string)
   description = "List of public subnets inside VPC"
   default = {
     public-1 = "10.42.10.0/24"
     public-2 = "10.42.11.0/24"
-      }
+  }
 }
